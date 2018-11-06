9a5101d332af3c6de200d53497afaf110f862593
mosnyder <Morgan.Snyder@usiouxfalls.edu>
2018-11-05 20:48:53 -0600

    initial commit

---
+++ app/src/main/java/apps/snyder/mini_arcade/MainMenu.java
@@ -0,0 +1,13 @@
+package apps.snyder.mini_arcade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
